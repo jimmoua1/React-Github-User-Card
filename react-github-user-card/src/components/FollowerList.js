54bb7680546c90df1a9784ed241b8085b83fe172
jimmoua1 <tsheejmaus@yahoo.com>
2020-08-07 17:19:21 -0700

    fix some minor problem

--- react-github-user-card/src/components/FollowerList.js
+++ react-github-user-card/src/components/FollowerList.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
+import FollowerCard from '../components/FollowerCard';
 
 class FollowerList extends Component {
   constructor() {
